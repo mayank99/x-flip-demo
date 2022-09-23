bdd89c473a29b5362611ad3f80a08c21f36308b1
Mayank <mayank99@users.noreply.github.com>
2022-09-22 23:32:05 -0400

    Format

--- src/components/x-flip.ts
+++ src/components/x-flip.ts
@@ -3,33 +3,24 @@ export class XFlip extends HTMLElement {
 
 	public async play(action: () => void) {
 		const first = this.getBoundingClientRect();
-		action();
 
-		// wait for next frame
-		await new Promise((resolve) => setTimeout(resolve));
+		action();
+		await new Promise((resolve) => setTimeout(resolve)); // wait for next frame
 
 		const last = this.getBoundingClientRect();
 
-		const deltaX = first.left - last.left;
-		const deltaY = first.top - last.top;
-		const deltaW = first.width / last.width;
-		const deltaH = first.height / last.height;
+		const dX = first.left - last.left;
+		const dY = first.top - last.top;
+		const dW = first.width / last.width;
+		const dH = first.height / last.height;
+		const transform = `translate(${dX}px, ${dY}px) scale(${dW}, ${dH})`;
 
 		this.animate(
 			[
-				{
-					transformOrigin: 'top left',
-					transform: `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`,
-				},
-				{
-					transformOrigin: 'top left',
-					transform: 'none',
-				},
+				{ transformOrigin: 'top left', transform },
+				{ transformOrigin: 'top left', transform: 'none' },
 			],
-			{
-				duration: 300,
-				easing: 'ease-in-out',
-			}
+			{ duration: 300, easing: 'ease-in-out' }
 		);
 	}
 }
